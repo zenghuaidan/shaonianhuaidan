fe43a96551935e4616d02eff9a84b8d7a60f5270
zengyuanek <zengyuanke@163.com>
2018-02-10 15:20:58 +0800

    log size

--- src/main/java/com/yoson/task/BackTestTask.java
+++ src/main/java/com/yoson/task/BackTestTask.java
@@ -209,6 +209,7 @@ public class BackTestTask implements Runnable {
 				BackTestTask.allBTTradeResults.clear();
 				BackTestTask.allBTSummaryResults.clear();
 				BackTestTask.allPositivePnlResult.clear();
+				BackTestTask.allProfitAndLossResults = new StringBuilder();
 				BackTestCSVWriter.writeText(mainUIParam.getStepPath(), index + "," + testSets.size(), false);
 				first = false;
 			}
@@ -240,6 +241,7 @@ public class BackTestTask implements Runnable {
 		BackTestTask.rowData = null;
 		BackTestTask.sortedDateList = null;
 		BackTestTask.allPositivePnlResult = null;
+		BackTestTask.allProfitAndLossResults = null;
 		System.gc();
 		Runtime.getRuntime().freeMemory();
 	}
