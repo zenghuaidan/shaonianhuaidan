7715165452a20299f365165d0b3dbd8d87cb151b
larry <larry.zeng@edeas.hk>
2018-02-21 21:26:10 +0800

    pnl by date

--- src/main/java/com/yoson/csv/BackTestCSVWriter.java
+++ src/main/java/com/yoson/csv/BackTestCSVWriter.java
@@ -3,18 +3,23 @@ package com.yoson.csv;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.FalseFileFilter;
+import org.directwebremoting.util.FakeHttpServletRequest;
 
+import com.opencsv.CSVReader;
 import com.yoson.chart.chartWriter;
 import com.yoson.date.DateUtils;
 import com.yoson.model.BackTestResult;
@@ -172,14 +177,6 @@ public class BackTestCSVWriter {
 		}
 		BackTestTask.allProfitAndLossResults.append(id + "," + backTestResult.testSet.getKey() + ",");
 		for (PerDayRecord perDayRecord : backTestResult.dayRecords) {
-			Long dateLong = perDayRecord.getDate().getTime();
-			if(!BackTestTask.allProfitAndLossResultMap.containsKey(dateLong) || (Double)BackTestTask.allProfitAndLossResultMap.get(dateLong).get(0) < perDayRecord.totalPnL) {
-				List<Object> pnlInfo = new ArrayList<Object>();
-				pnlInfo.add(perDayRecord.totalPnL);
-				pnlInfo.add(id);
-				pnlInfo.add(backTestResult.testSet.getKey());
-				BackTestTask.allProfitAndLossResultMap.put(dateLong, pnlInfo);
-			}
 			BackTestTask.allProfitAndLossResults.append(perDayRecord.totalPnL + ",");
 			
 			pnlContent.append(backTestResult.testSet.getKey() + ",")
@@ -209,55 +206,126 @@ public class BackTestCSVWriter {
 		BackTestTask.allProfitAndLossResults.append("\n");
 	}
 	
-	public static String getBestPnlByDate() {
+	public static String getBestPnlByDate(MainUIParam mainUIParam) {
 		StringBuilder content = new StringBuilder();
 		content.append("Date,Test no.,key,Total Pnl").append("\n");
-		if(BackTestTask.allProfitAndLossResultMap != null) {
-			for(Long dateLong : BackTestTask.allProfitAndLossResultMap.keySet()) {
-				List<Object> pnlInfo = BackTestTask.allProfitAndLossResultMap.get(dateLong);
-				Date date = new Date(dateLong);
-				String dateStr = DateUtils.yyyyMMdd().format(date);
+		try {
+			Map<String, List<Object>> allProfitAndLossResultMap = new TreeMap<String, List<Object>>();
+			File file = new File(FilenameUtils.concat(mainUIParam.getSourcePath(), BackTestCSVWriter.profitAndLossFileName));
+			CSVReader csvReader = new CSVReader(new FileReader(file), ',', '\n', 0);	
+			String [] lines;
+			boolean first = true;
+			List<String> dates = new ArrayList<String>();
+			while ((lines = csvReader.readNext()) != null)  {
+				if (first) {
+					for(int i = 2; i < lines.length - 1; i++) {
+						dates.add(lines[i]);
+					}	
+					first = false;
+				} else {
+					String id = lines[0]; 
+					String key = lines[1];
+					for(int i = 2; i < lines.length - 1; i++) {
+						String dateStr = dates.get(i - 2);
+						double totalPnl = Double.parseDouble(lines[i]);
+						if(!allProfitAndLossResultMap.containsKey(dateStr) || (Double)allProfitAndLossResultMap.get(dateStr).get(0) < totalPnl) {
+							List<Object> pnlInfo = new ArrayList<Object>();
+							pnlInfo.add(totalPnl);
+							pnlInfo.add(id);
+							pnlInfo.add(key);
+							allProfitAndLossResultMap.put(dateStr, pnlInfo);
+						}
+					}
+				}
+			}
+			csvReader.close();
+						
+			for(String dateStr : allProfitAndLossResultMap.keySet()) {
+				List<Object> pnlInfo = allProfitAndLossResultMap.get(dateStr);				
 				content.append(dateStr + "," + pnlInfo.get(1) + "," + pnlInfo.get(2) + "," + pnlInfo.get(0)).append("\n");
 			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return content.toString();
 	}
 	
-	public static String getBestPnlBySpecifyDates(Set<Integer> specifyDateRanges) {
+	public static String getBestPnlBySpecifyDates(Set<Integer> specifyDateRanges, MainUIParam mainUIParam) {
 		StringBuilder content = new StringBuilder();
 		if(specifyDateRanges != null && specifyDateRanges.size() > 0) {
 			content.append("Date,");
 			for(int specifyDateRange : specifyDateRanges) {
-				content.append("," + "n=" + specifyDateRange + " Test no.," + "n=" + specifyDateRange + " key," + "n=" + specifyDateRange + " Total Pnl,");
+				content.append("," + "n=" + specifyDateRange + " Test no.," + "n=" + specifyDateRange + " key," + "n=" + specifyDateRange + " Sum of Total Pnl," + "n=" + specifyDateRange + " Total Pnl,");
 			}
 			content.append("\n");
-			if(BackTestTask.allProfitAndLossResultMap != null) {
-				List<List<Object>> allProfitAndLossResultList = new ArrayList<List<Object>>();
-				for(Long dateLong : BackTestTask.allProfitAndLossResultMap.keySet()) {
-					allProfitAndLossResultList.add(BackTestTask.allProfitAndLossResultMap.get(dateLong));
+			
+			List<String> dates = new ArrayList<String>();
+			Map<String, List<List<Object>>> allProfitAndLossResultMap = new TreeMap<String, List<List<Object>>>();
+			try {
+				File file = new File(FilenameUtils.concat(mainUIParam.getSourcePath(), BackTestCSVWriter.profitAndLossFileName));
+				CSVReader csvReader = new CSVReader(new FileReader(file), ',', '\n', 0);	
+				String [] lines;
+				boolean first = true;
+				while ((lines = csvReader.readNext()) != null)  {
+					if (first) {
+						for(int i = 2; i < lines.length - 1; i++) {
+							dates.add(lines[i]);
+						}
+						first = false;
+					} else {
+						String id = lines[0]; 
+						String key = lines[1];
+						for(int i = 2; i < lines.length - 1; i++) {
+							String dateStr = dates.get(i - 2);
+							double totalPnl = Double.parseDouble(lines[i]);
+							List<Object> pnlInfo = new ArrayList<Object>();
+							pnlInfo.add(totalPnl);
+							pnlInfo.add(id);
+							pnlInfo.add(key);
+							if(allProfitAndLossResultMap.containsKey(dateStr)) {
+								allProfitAndLossResultMap.get(dateStr).add(pnlInfo);
+							} else {
+								List<List<Object>> list = new ArrayList<List<Object>>();
+								list.add(pnlInfo);
+								allProfitAndLossResultMap.put(dateStr, list);
+							}
+						}
+					}
 				}
+				csvReader.close();							
+												
 				int i = 1;
-				for(Long dateLong : BackTestTask.allProfitAndLossResultMap.keySet()) {
-					Date date = new Date(dateLong);
-					String dateStr = DateUtils.yyyyMMdd().format(date);
+				for(String dateStr : allProfitAndLossResultMap.keySet()) {					
 					content.append(dateStr + ",");
 					for(int specifyDateRange : specifyDateRanges) {
 						if(specifyDateRange >= i) {
-							content.append(",0,0,0,");							
+							content.append(",0,0,0,0,");							
 						} else {
-							int j = i - specifyDateRange - 1;
-							List<Object> maxPnlInfo = allProfitAndLossResultList.get(j);
-							for(; j < i - 1; j++) {
-								if((double)maxPnlInfo.get(0) < (double)allProfitAndLossResultList.get(j).get(0)) {
-									maxPnlInfo = allProfitAndLossResultList.get(j);
+							Double maxSumOfPnl = null;
+							int maxId = 0;
+							for(int id = 0; id < allProfitAndLossResultMap.get(dateStr).size(); id++) {
+								double sumOfPnl = 0;
+								int j = i - specifyDateRange - 1;							
+								for(; j < i - 1; j++) {
+									String _dateStr = dates.get(j);
+									double totalPnl = (double)allProfitAndLossResultMap.get(_dateStr).get(id).get(0);
+									sumOfPnl += totalPnl;
+								}
+								if (maxSumOfPnl == null || maxSumOfPnl < sumOfPnl) {
+									maxSumOfPnl = sumOfPnl;
+									maxId = id;
 								}
 							}
-							content.append("," + maxPnlInfo.get(1) + "," + maxPnlInfo.get(2) + "," + maxPnlInfo.get(0) + ",");
+							
+							content.append("," + allProfitAndLossResultMap.get(dateStr).get(maxId).get(1) + "," + allProfitAndLossResultMap.get(dateStr).get(maxId).get(2) + "," + maxSumOfPnl + "," + allProfitAndLossResultMap.get(dateStr).get(maxId).get(0) + ",");
 						}						
 					}
 					content.append("\n");
 					i++;
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 		return content.toString();
