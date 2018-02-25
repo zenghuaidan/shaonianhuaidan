e7df2ce16df5d424aad0bc0c3478f9a58c1cb835
zengyuanek <zengyuanke@163.com>
2018-02-25 10:37:00 +0800

    auto upload data

--- src/main/java/com/yoson/scheduler/TaskScheduler.java
+++ src/main/java/com/yoson/scheduler/TaskScheduler.java
@@ -1,28 +1,14 @@
 package com.yoson.scheduler;
 
-import java.io.File;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.filefilter.FalseFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.lang3.StringUtils;
 
 import com.ib.client.Contract;
 import com.yoson.cms.controller.IndexController;
-import com.yoson.sql.SQLUtils;
 import com.yoson.tws.EClientSocketUtils;
-import com.yoson.tws.RawDataCSVWriter;
-import com.yoson.tws.Record;
-import com.yoson.tws.ScheduledDataCSVWriter;
-import com.yoson.tws.ScheduledDataRecord;
 import com.yoson.tws.YosonEWrapper;
-import com.yoson.web.InitServlet;
 
 public class TaskScheduler {
 	public synchronized void doTrade() throws ParseException {
