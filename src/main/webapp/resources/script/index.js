fe22b63c351aad6786f7dbe6c64c70756df4e465
zengyuanek <zengyuanke@163.com>
2018-05-27 13:51:12 +0800

    matrixFile

--- src/main/webapp/resources/script/index.js
+++ src/main/webapp/resources/script/index.js
@@ -388,7 +388,7 @@ function getMainUIParam() {
 	   "includeMorningData":$("[name='includeMorningData']").is(":checked"),
 	   "ignoreLunchTime":$("[name='ignoreLunchTime']").is(":checked"),	
 	   "nForPnl":$("[name='nForPnl']").val(),
-	   "matrixFile":$("[name='ignoreLunchTime']").is(":checked")
+	   "matrixFile":$("[name='matrixFile']").is(":checked")
 	};
 }
 
