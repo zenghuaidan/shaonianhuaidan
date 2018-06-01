dea8c8eb49f736b21f26021451c9e8af662ce2bc
larry <larry.zeng@edeas.hk>
2018-06-01 16:43:21 +0800

    includeMorningData

--- src/main/java/com/yoson/model/MainUIParam.java
+++ src/main/java/com/yoson/model/MainUIParam.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.google.gson.annotations.Expose;
