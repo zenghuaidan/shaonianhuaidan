f764e1d81c7cbba96c76c3f79a1d9b63dbeb6923
zengyuanek <zengyuanke@163.com>
2018-06-04 22:02:26 +0800

    ignoreLunchTime

--- src/main/java/com/yoson/model/MainUIParam.java
+++ src/main/java/com/yoson/model/MainUIParam.java
@@ -467,10 +467,10 @@ public class MainUIParam extends TestSet {
 	}
 	
 	public static MainUIParam loadMainUIParamFromCSV(File strategyFile) {
+		MainUIParam mainUIParam = new MainUIParam();
 		try {
 			CSVReader csvReader = new CSVReader(new FileReader(strategyFile), ',', '\n', 0);
 			String [] lines;
-			MainUIParam mainUIParam = new MainUIParam();
 			List<String []> params = new ArrayList<String []>();
 			int index = 0;
 			while ((lines = csvReader.readNext()) != null && index <= 200 )  {
@@ -505,10 +505,9 @@ public class MainUIParam extends TestSet {
 			mainUIParam.setIncludeMorningData(Boolean.parseBoolean(params.get(index++)[1]));
 			mainUIParam.setIgnoreLunchTime(Boolean.parseBoolean(params.get(index++)[1]));
 			csvReader.close();
-			return mainUIParam;
 		} catch (Exception e) {
-			return null;
 		}
+		return mainUIParam;
 	}
 	
 	public int isCheckMarketTime(String timeStr) throws ParseException {
