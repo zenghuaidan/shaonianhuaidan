fe2cfb082012aabd7c34d67b72bc8d376ef8147b
larry <larry.zeng@edeas.hk>
2019-01-28 09:49:48 +0800

    refactor

--- src/main/java/com/yoson/scheduler/TaskScheduler.java
+++ src/main/java/com/yoson/scheduler/TaskScheduler.java
@@ -2,14 +2,11 @@ package com.yoson.scheduler;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
-import javax.validation.constraints.Null;
 
 import org.apache.commons.lang3.StringUtils;
 
